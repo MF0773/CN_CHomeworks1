ceb66956b32e7d84a161b53fd884e9d867c76275
c0re <mohammad.fateh256@ut.ac.ir>
2022-12-31 17:44:44 +0330

    sample added

--- FTP/client/client_main.cpp
+++ FTP/client/client_main.cpp
@@ -15,6 +15,30 @@ class FtpClient{
         
     }
 
+    int sampleConnect(int port) {
+        int fd;
+        struct sockaddr_in server_address;
+        
+        fd = socket(AF_INET, SOCK_STREAM, 0);
+        
+        server_address.sin_family = AF_INET; 
+        server_address.sin_port = htons(port); 
+        server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+        if (connect(fd, (struct sockaddr *)&server_address, sizeof(server_address)) >= 0) { // checking for errors
+            controlFd = fd;
+            controlPort = port;
+
+            clog<< "connected on port : "<< controlPort;
+            string buff = "Hello,Iam new client";
+            send(controlFd, buff.c_str(), buff.size(), 0);
+            return true;
+        }
+
+        clog<< "could not connected on port : "<< controlPort;
+        return false;        
+    }
+
     bool connectToServer(int port){
         struct sockaddr_in serverAdrr;
         sockaddr_in serverAdrress;
@@ -28,7 +52,8 @@ class FtpClient{
         result = connect( controlFd,(sockaddr *)&serverAdrr, sizeof(serverAdrr) ) ;
         if ( result >= 0 ) {
             clog<< "connected on port : "<< controlPort;
-            write(controlFd,"s",2);
+            string buff = "Hello,Iam new client";
+            send(controlFd, buff.c_str(), buff.size(), 0);
             return true;
         }
         else{
@@ -46,7 +71,8 @@ class FtpClient{
 int main(int argc,char **argv){
     cout<<"client started"<<endl;
     FtpClient client;
-    client.connectToServer(2121);
+    // client.connectToServer(2121);
+    client.sampleConnect(2121);
     client.disconnectFromServer();
     return 0;
 }
