a4dbf9cf875b4e69d189d67292b26322ca276541
Iran <Ali@gmail.com>
2023-01-11 15:58:43 +0330

    add serverMain

---
+++ Chatroom/serverMain.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+
+#include "msgStruct.hpp"
+
+int start_server(int port)
+{
+    struct sockaddr_in address;
+    int server_fd;
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+
+    listen(server_fd, 4);
+
+    return server_fd;
+}
+
+int main(int argc, char const *argv[])
+{
+    int server_fd, client_fd;
+    char buff[1024] = {0};
+    char username_
+
+    if (argc >= 3)
+    {
+        server_fd = start_server(atoi(argv[1]));
+
+        printf("%d\n", atoi(argv[1]));
+    }
+
+}
