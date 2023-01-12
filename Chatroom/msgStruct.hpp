9b1df2a56a060be6ee816e313e383d53a8f03cad
Iran <Ali@gmail.com>
2023-01-12 23:52:36 +0330

    add encoder

--- Chatroom/msgStruct.hpp
+++ Chatroom/msgStruct.hpp
@@ -27,7 +27,7 @@ union msgStruct
     } M;
 };
 
-void initialMSG(msgStruct &msg, MessageType _mess_type, int _mess_id, int _length, char *_payload)
+void initialMSG(msgStruct &msg, MessageType _mess_type, const int &_mess_id, const int &_length, char *_payload)
 {
     memset(msg.buff, 0, SIZE_BUFF);
     msg.M.mess_type = _mess_type;
@@ -35,3 +35,44 @@ void initialMSG(msgStruct &msg, MessageType _mess_type, int _mess_id, int _lengt
     msg.M.length = _length;
     strcpy(msg.M.payload, _payload);
 };
+
+void inline initial_CONNECT(msgStruct &msg, const int &_mess_id, char *_payload)
+{
+    initialMSG(msg, CONNECT, _mess_id, 2 + strlen(_payload), _payload);
+}
+void inline initial_CONNACK(msgStruct &msg, const int &_mess_id)
+{
+    initialMSG(msg, CONNACK, _mess_id, 2, nullptr);
+}
+void inline initial_LIST(msgStruct &msg, const int &_mess_id)
+{
+    initialMSG(msg, LIST, _mess_id, 2, nullptr);
+}
+void inline initial_LISTREPLY(msgStruct &msg, const int &_mess_id, const int &n, char *_payload)
+{
+    initialMSG(msg, LISTREPLY, _mess_id, 2 + 2 * n, _payload);
+}
+void inline initial_INFO(msgStruct &msg, const int &_mess_id, char *_payload)
+{
+    initialMSG(msg, INFO, _mess_id, 2 + 2, _payload);
+}
+void inline initial_INFOREPLY(msgStruct &msg, const int &_mess_id, char *_payload)
+{
+    initialMSG(msg, INFOREPLY, _mess_id, 2 + strlen(_payload), _payload);
+}
+void inline initial_SEND(msgStruct &msg, const int &_mess_id, char *_userID, char *_message)
+{
+    initialMSG(msg, SEND, _mess_id, 2 + 2 + strlen(_message), strcat(_message, _userID));
+}
+void inline initial_SENDREPLY(msgStruct &msg, const int &_mess_id, bool isSuccess)
+{
+    initialMSG(msg, SENDREPLY, _mess_id, 2 + 1, (isSuccess ? '0' : '1'));
+}
+void inline initial_RECEIVE(msgStruct &msg, const int &_mess_id, bool isSuccess)
+{
+    initialMSG(msg, RECEIVE, _mess_id, 2, nullptr);
+}
+void inline initial_RECEIVEREPLY(msgStruct &msg, const int &_mess_id, char *_sendID, char *_message)
+{
+    initialMSG(msg, RECEIVEREPLY, _mess_id, 2 + 2 + strlen(_message), strcat(_message, _sendID));
+}
